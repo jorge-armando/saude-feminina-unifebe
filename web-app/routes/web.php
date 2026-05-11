e15e9df4e5064a21a25f31d0bd6fa25486a456de
ZancaNeto <herciliozanca@gmail.com>
2026-05-11 20:01:16 -0300

    routes and views

--- web-app/routes/web.php
+++ web-app/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/painel', function () {
+    return view('dashboard');
+});
+
+Route::get('/novo-conteudo', function () {
+    return view('create');
 });
