50b4ce24ca0330d365c6d88ac2026ab48773cef0
Jorge Armando <jorge@magemontreal.com>
2026-05-12 19:37:18 -0300

    Modal

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,5 +1,18 @@
+import Menu from "@/components/Menu";
 import { Stack } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <View style={styles.container}>
+      <Stack />
+      <Menu />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
